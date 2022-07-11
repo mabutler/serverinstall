56531a479a379a4133e1ff269d67260c6f959c87
Matt Butler <matt@mabutler.us>
2022-07-11 19:50:45 -0400

    Add authorized keys and nginx sites

--- install.sh
+++ install.sh
@@ -1,2 +1,30 @@
 #!/bin/sh
-pacman -S --needed --noconfirm $(cat package.list)
+
+source ./variables.list
+
+# full update before anything else
+pacman -Syu --noconfirm
+
+# disable any default services we don't want
+for service in $(cat services.disable.list)
+do
+	systemctl disable $service --now
+done
+
+# install all packaes we need
+pacman -S --needed --noconfirm $(cat packages.install.list)
+
+# remove any default packages we don't need
+pacman -Rs --noconfirm $(cat packages.remove.list)
+
+# install nginx sites
+cp -r ./nginx/* /etc/nginx/
+
+# enable all new services we do want
+for service in $(cat services.enable.list)
+do
+	systemctl enable $service --now
+done
+
+certbot --test-cert -n --nginx --agree-tos --email $EMAIL --expand --domains $DOMAINS
+
